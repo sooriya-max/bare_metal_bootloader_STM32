5dd8749c610a9b59bb3ca4ba6201d307cdc7ed6a
sooriya-max <sooriya5302@gmail.com>
2026-07-03 21:25:46 +0530

    Built and Added a Startup File in C

---
+++ src/Startup_c.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+extern uint32_t _sidata, _sdata, _edata; // for copying flash data to .data
+extern uint32_t _sbss, _ebss;            // for zeroing out .bss
+
+int main(void);
+
+void Reset_Handler(void)
+{
+    //copying Flash data to .data
+    
+    uint32_t *src = &_sidata;
+    uint32_t *dst = &_sdata;
+    
+    while(dst < &_edata)
+    {
+        *dst++ = *src++;
+    }
+    
+    //Zeroing .bss Section
+    
+    uint32_t *e_src = &_sbss;
+    
+    while(e_src < &_ebss)
+    {
+        *e_src++ = 0;
+    }
+    
+    //Calling Main Function
+    main(); 
+}
