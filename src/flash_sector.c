3ba0a322b268f926412f563593c8c66bc9b79f1e
sooriya-max <sooriya5302@gmail.com>
2026-07-06 23:57:07 +0530

    Code Cleanup

--- src/flash_sector.c
+++ src/flash_sector.c
@@ -1,24 +1,23 @@
 #include <stdint.h>
 #include "flash_sector.h"
 
+#define FLASH_BASE ((volatile uint32_t *)0x40023C00)
 
 void flash_unlock(void)
 {
-    volatile uint32_t *Flash_baseAddr = (0x40023C00);
-    volatile uint32_t *FLASH_KEYR = Flash_baseAddr + 1;
-    volatile uint32_t *FLASH_CR = Flash_baseAddr + 4;
+    volatile uint32_t *FLASH_KEYR = FLASH_BASE + 1;
+    volatile uint32_t *FLASH_CR = FLASH_BASE + 4;
     
-    *FLASH_KEYR = 0x45670123;
-    *FLASH_KEYR = 0xCDEF89AB;
+    *FLASH_KEYR = 0x45670123;   //Writing the First Key into the Control Register
+    *FLASH_KEYR = 0xCDEF89AB;   //Writing the Second Key into the Control Register
     
 
 }
 
 void flash_erase_sector(uint8_t sector)
 {
-    volatile uint32_t *Flash_baseAddr = (0x40023C00);
-    volatile uint32_t *FLASH_SR = Flash_baseAddr + 3;
-    volatile uint32_t *FLASH_CR = Flash_baseAddr + 4;
+    volatile uint32_t *FLASH_SR = FLASH_BASE + 3;
+    volatile uint32_t *FLASH_CR = FLASH_BASE + 4;
     *FLASH_CR |= (1U << 1);
     
     *FLASH_CR &= ~(0xF << 3);   //For Clearing the Sector;
@@ -31,10 +30,9 @@ void flash_erase_sector(uint8_t sector)
 
 void flash_lock(void)
 {
-    volatile uint32_t *Flash_baseAddr = (0x40023C00);
-    volatile uint32_t *FLASH_CR = Flash_baseAddr + 4;
+    volatile uint32_t *FLASH_CR = FLASH_BASE + 4;
     
-    *FLASH_CR |= (1U << 31);
+    *FLASH_CR |= (1U << 31);    //Locking the Flash after Clearing
     
     
 }
