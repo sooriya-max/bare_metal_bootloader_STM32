96eb049004ee90a321fbaabeaa69419aec1e341b
sooriya-max <sooriya5302@gmail.com>
2026-07-07 21:24:28 +0530

    Added flash_write test in main.c — erase sector 2, write known pattern

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
-int x = 10, y; //x is for .data, y is for .bss
-short z;       //z is added to check the size allocation for the int and short
+#include <stdint.h>
+#include "flash_sector.h"
 
 
 int main()
 {
-    int z = x + y;
-    return 0; // Dummy function that does nothing
+    const uint32_t src[] = {0xDEADBEEF, 0xCAFEBABE, 0x12345678, 0xABCDABCD};
+    volatile uint32_t * dest = (volatile uint32_t *)0x08008000;     //Sector 2 Address
+    uint32_t length = 4;
+
+    flash_unlock();
+    flash_erase_sector(2);          //Erasing Sector 2
+    flash_write(dest, src, length);
+    flash_lock();
+    
+    return 0; 
 }
