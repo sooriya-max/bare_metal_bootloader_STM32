b74775686965d3423bc8cdaf4ccd656d1b48f89a
sooriya-max <sooriya5302@gmail.com>
2026-07-09 23:15:12 +0530

    update on the mini update

--- src/uart.c
+++ src/uart.c
@@ -2,19 +2,22 @@
 
 void uart_init(void)
 {
-    // Step 1 — enable USART2 clock via RCC
-    volatile uint32_t *RCC_BASE_ADDR = (volatile uint32_t *)0x40023800;
-    volatile uint32_t *RCC_APB1ENR   = RCC_BASE_ADDR + 16;
-    *RCC_APB1ENR |= (1U << 17);
+    // 1. Enable clocks first
+volatile uint32_t *RCC_BASE_ADDR = (volatile uint32_t *)0x40023800;
+volatile uint32_t *RCC_AHB1ENR  = RCC_BASE_ADDR + 12;  // GPIOA clock
+volatile uint32_t *RCC_APB1ENR  = RCC_BASE_ADDR + 16;  // USART2 clock
+*RCC_AHB1ENR |= (1U << 0);
+*RCC_APB1ENR |= (1U << 17);
 
-    // Step 2 — load BRR
-    volatile uint32_t *USART2_BRR = ;
-    *USART2_BRR = 0x8AE;
+// 2. Configure PA2 as AF7
+volatile uint32_t *GPIO_A_BASE = (volatile uint32_t *)0x40020000;
+volatile uint32_t *MODER       = GPIO_A_BASE + 0;       //This is to Say that this is Alternating Function
+volatile uint32_t *AFRL        = GPIO_A_BASE + 8;       //This is to Say that This is for UART_TX
+*MODER |= (0b10 << 4);
+*AFRL  |= (0b0111 << 8);
 
-    // Step 3 — enable USART2 and TX via CR1
-
-
-    // Step 4 — uart_write() polling loop
+// 3. Configure USART2
+// BRR, CR1 — next
 }
 
 
