2e59bf203295d20c903236ccc46384a639da84d9
sooriya-max <sooriya5302@gmail.com>
2026-07-14 22:29:11 +0530

    Added UART Reieve Image function in main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include "flash_sector.h"
 #include "uart.h"
 
+#define FIRMWARE_MAX_SIZE (96 * 1024)
+uint8_t recv_buf[FIRMWARE_MAX_SIZE];
+
 int main()
 {
     const uint32_t src[] = {0xDEADBEEF, 0xCAFEBABE, 0x12345678, 0xABCDABCD};
@@ -24,10 +27,49 @@ int main()
 
     //This is a Echo Test with a single character from Rx to Tx
     uint8_t ch = uart_getc();
-    uart_puts("UART CHARACTER ");
+    uart_puts("UART CHARACTER <");
     uart_write(ch);
-    uart_puts(" GOT IT");    
+    uart_puts("> GOT IT");    
 
     
     return 0; 
+}
+
+uint32_t uart_receive_image(uint8_t *buf, uint32_t max_size)
+{
+    // wait for 0x69 Start bit
+    while(!(uart_getc() == 0x69));
+
+    //Getting the 4-byte length component
+    uint32_t length = 0;
+    uint8_t iterator = 0;
+    while(iterator < 4)
+    {
+        length |= ((uint32_t)uart_getc() << (iterator * 8));
+        iterator++;
+    }
+
+    //Getting the Length Field into buf
+    if(length > FIRMWARE_MAX_SIZE)
+    {
+        return 0;               //This 0 will go to the length fields of flash write saying that this is a failure
+    }
+    iterator = 0;
+    while(iterator < length)
+    {
+        buf[iterator]= uart_getc();
+        iterator++;
+    }
+    
+    //Getting the CRC word
+    uint32_t CRC_length = 0;
+    iterator = 0;
+    while(iterator < 4)
+    {
+        CRC_length |= ((uint32_t)uart_getc() << (iterator * 8));
+        iterator++;
+    }
+
+    //Returning the Length parameter
+    return length;    
 }
